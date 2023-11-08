f78368c605fdde1f290bfe99c57e9f05b91ad7fa
Matteito1 <matteo.vilchez.7e6@itb.cat>
2023-11-08 16:04:27 +0100

    8 de Noviembre

--- e4CustomerLoyaltyCard.py
+++ e4CustomerLoyaltyCard.py
@@ -12,11 +12,12 @@ en cas contrari no se li aplica cap descompte. El descompte es calcula al preu f
 I no a la “base imponible”. I després si li aplica l’IVA
 Precio total = Base imponible * Tipo de IVA
 """
-Precio = float(input("Introduzca el valor"))
-tarjeta= input("Tiene usted tarjeta:")
-if tarjeta==False:
+Precio = float(input("Introduzca el valor:"))
+tarjeta= input("Tiene usted tarjeta (si/no):")
+if tarjeta =="no":
     print("No tiene tarjeta asi que su precio será sin descuento y será:",Precio,"€")
-if Precio >= 100:
+elif tarjeta == "si":
+   if Precio >= 100:
     PrecioMod = Precio+21
     PrecioDescont = PrecioMod*0.79
     PrecioIva = PrecioDescont*1.21
--- e5IsACorrectDate.py
+++ e5IsACorrectDate.py
@@ -8,8 +8,21 @@ El programa ha de demanar una data, dia, mes i any (en el format DD/MM/AAAA).
 Cal recordar que hi ha anys de traspàs.
 NO es pot fer servir funcions de calendari com ara datetime de Python
 """
-fecha = input()
-fecha = fecha.split()
-dia = int(fecha[0])
-mes = int(fecha[1])
-any = int(fecha[2])
+dia = input("Introduce el día: ")
+mes = input("Introduce el mes: ")
+any = input("Introduce el año: ")
+if mes == "01" or mes == "03" or mes == "05" or mes == "07" or mes == "08" or mes == "10" or mes == "12":
+    if int(dia) > 31:
+        print("Fecha incorrecta")
+elif mes == "04" or mes == "06" or mes == "09" or mes == "11":
+    if int(dia) > 30:
+        print("Fecha incorrecta")
+elif mes == "02":
+    if int(dia) > 28:
+        if int(any) % 4 == 0 and (int(any) % 100 != 0 or int(any) % 400 == 0):
+            if int(dia) > 29:
+                print("Fecha incorrecta")
+if int(any) < 1 or int(any) > 9999:
+    print("Fecha incorrecta")
+else:
+    print("Fecha correcta")
